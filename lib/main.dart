e11ad9df6e7df9c9cede8975488aa36a675b265e
Aleksejs Muzicenko <a.muzicenko@multipass.co>
2022-08-24 15:46:46 +0300

    first changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('flutter first try'),
-    ));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.red),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter'),
+            centerTitle: true,
+          ),
+          body: const Center(
+            child: Text('flutter_first_try',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blue,
+                    fontFamily: 'Timer New Roman')),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: const Text('Click me'),
+            backgroundColor: Colors.red,
+            onPressed: () => {print('Clicked')},
+          ),
+        ));
+  }
+}
